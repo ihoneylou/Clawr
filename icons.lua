d04fbed3749d182e6043bafc3c06cc9628ffcdfd
honeylou <nbonney261@gmail.com>
2026-04-06 16:24:30 +0100

    added moreeee

---
+++ icons.lua
@@ -0,0 +1,29 @@
+-- Tab Icons, you can change these to whatever you want by copying the Texture ID of a decal. 
+-- Tested and working as of 06/04/26, also updated DAILY.
+-- Follow "ihoneylou" on tiktok! - These are all found manually would help alot.
+------------------------------------------------------------------
+rbxassetid://14488863746 -- Star Icon
+rbxassetid://13300915301 -- Settings Icon
+rbxassetid://117259180607823 -- Player/Human Icon
+rbxassetid://76499042599127 -- Sword Icon
+rbxassetid://138880939782808 -- Star II Icon
+rbxassetid://87273393473760 -- Sparkle Icon
+rbxassetid://124620632231839 -- Home/House Icon
+rbxassetid://11322089611 -- Crown Icon
+rbxassetid://7059338404 -- Music Icon
+rbxassetid://16060788318 -- Camera Icon
+rbxassetid://13321880274 -- Power Icon
+rbxassetid://15875255096 -- Eye Icon
+rbxassetid://11348555035 -- Code Icon
+rbxassetid://78388764182737 -- Folder Icon
+rbxassetid://120426282375540 -- Palette/Paint Icon
+rbxassetid://128003672551215 -- Tag Icon
+rbxassetid://99625725727957 -- Link Icon
+rbxassetid://99761949668282 -- Clock Icon
+rbxassetid://97651580914952 -- Calender Icon
+rbxassetid://104170030929097 -- Warning Icon
+rbxassetid://110789965791116 -- Question Mark in a Circle Icon
+rbxassetid://15828140488 -- Info Icon
+rbxassetid://89015294175898 -- Heart Icon
+rbxassetid://7461510428 -- Sheild Icon
+rbxassetid://84693206442598 -- Key Icon
